3e554eb335f5bc80a8e9faf7a5cece62b023346c
ayrwl <alston.yam@gmail.com>
2024-06-28 23:01:43 +1200

    did i not already commit this why is there an error wtf

--- Squareroot.playground/Contents.swift
+++ Squareroot.playground/Contents.swift
@@ -1,3 +1,27 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+enum errors: Error {
+    case bound, noroot
+}
+
+func getSqrt(num: Int) throws -> Int {
+    if num > 10000 || num < 1 {
+        throw errors.bound
+    }
+    for i in 1...100 {
+        if i * i == num {
+            return i
+        }
+    }
+    
+    throw errors.noroot
+}
+
+do {
+    let result = try getSqrt(num: 64)
+    print(result)
+} catch errors.bound {
+    print("Out of Bounds")
+} catch errors.noroot {
+    print("There's no root")
+}
