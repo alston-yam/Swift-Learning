5036085624ff548cc284f411070782bf4cacf4c6
ayrwl <alston.yam@gmail.com>
2024-07-02 22:20:51 +1200

    ???

--- Closure.playground/Contents.swift
+++ Closure.playground/Contents.swift
@@ -1,3 +1,30 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+func greetUser() {
+    print("Hello World!")
+}
+
+greetUser()
+var greetCopy = greetUser
+greetCopy()
+
+// The "in" keyword marks the end of the parameter declarations
+let greet = { (name: String) -> String in
+    "Hi \(name)!"
+}
+
+print(greet("Bob"))
+
+let team = ["A", "B", "D", "C", "Z", "J"]
+func firstCaptainName(name1: String, name2: String) -> Bool {
+    if name1 == "D" {
+        return true
+    } else if name2 == "D" {
+        return false
+    }
+    
+    return name1 < name2
+}
+
+let captainFirstTeam = team.sorted(by: firstCaptainName)
+print(captainFirstTeam)
